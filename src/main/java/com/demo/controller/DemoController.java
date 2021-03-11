f68d3491febdb9cd55bbf87c44ea0870c8801ee7
Benjamin Nieto <benjaminieto2011@gmail.com>
2021-03-11 08:27:42 -0500

    commit only forms

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -65,9 +65,6 @@ public class DemoController {
 	@Autowired
 	private ConfigProperty configs;
 
-//	@Autowired
-//	private MinioClient minioClient = 
-
 	@GetMapping("/")
 	public String testing() {
 		// service.registrar("foo");
@@ -75,114 +72,19 @@ public class DemoController {
 		return "ONLINE";
 	}
 
-	@GetMapping("/msg/testing")
-	public String msgTesting() {
-
-		return "ONLINE Path: /msg/testing";
+	@GetMapping("/api")
+	public String api() {
+		String ret = "Post /uploadfile/  -  key: file, type: body-file, value: file upload <br>";
+		ret = ret+"Get /cigee-galery/ - key: image, value: name-image-view <br>";
+		ret = ret+"Get /minio/info <br>";
+		ret = ret+"Get /minio/createBucket - key: name, value name-bucket-create <br>";
+		ret = ret+"Get /minio/list";
+		ret = ret+"Post /minio/uploadFile - key: bucket, value name-bucket-exist && key: name, value: name-file && key: file, type: body-file, value: file-upload";
+		ret = ret+"Get /minio/openFile - key: bucket, value: bucket-exist && key: name, value: name-to-preview";
+		
+		return ret;
 	}
 
-	@GetMapping("/date")
-	public String dateLocal() {
-		String retd = "";
-		Date date = new Date();
-		String strDateFormat = "dd/MM/YYYY hh:mm:ss a";
-		DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
-		retd = dateFormat.format(date);
-
-		return retd;
-	}
-
-	/*
-	 * @GetMapping("memory") public String kosMemory() throws InterruptedException {
-	 * int dummyArraySize = 15; String ret = "Max JVM memory: " +
-	 * Runtime.getRuntime().maxMemory() + "\n"; long memoryConsumed = 0; try {
-	 * long[] memoryAllocated = null; for (int loop = 0; loop < Integer.MAX_VALUE;
-	 * loop++) { memoryAllocated = new long[dummyArraySize]; memoryAllocated[0] = 0;
-	 * memoryConsumed += dummyArraySize * Long.SIZE; ret =
-	 * "Memory Consumed till now: " + memoryConsumed + "\n"; dummyArraySize *=
-	 * dummyArraySize * 2;
-	 * 
-	 * Thread.sleep(500); } } catch (OutOfMemoryError outofMemory) { ret =
-	 * "\n\nCatching out of memory error"; // Log the information,so that we can
-	 * generate the statistics (latter on). throw outofMemory; } return ret; }
-	 */
-
-	@GetMapping("/fail/memory")
-	public void kosMemory() throws Exception {
-		int iteratorValue = 20;
-		System.out.println("\n=================> OOM test started..\n");
-		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
-			System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
-			int loop1 = 2;
-			int[] memoryFillIntVar = new int[iteratorValue];
-			// feel memoryFillIntVar array in loop..
-			do {
-				memoryFillIntVar[loop1] = 0;
-				loop1--;
-			} while (loop1 > 0);
-			iteratorValue = iteratorValue * 5;
-			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
-			Thread.sleep(1000);
-		}
-	}
-
-	@GetMapping("whoami")
-	public String whoAmI(@RequestParam String command) throws Exception {
-
-		String s = null;
-		String print = "";
-
-		if (command.isEmpty()) {
-			command = "hostname";
-		}
-
-		try {
-
-			// run the Unix "ps -ef" command
-			// using the Runtime exec method:
-			Process p = Runtime.getRuntime().exec(command);
-
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-			// read the output from the command
-			LOG.info("Here is the standard output of the command:");
-			while ((s = stdInput.readLine()) != null) {
-				System.out.println(s);
-				print += s + " \n<br/>";
-			}
-
-			// read any errors from the attempted command
-			LOG.info("Here is the standard error of the command (if any):");
-			while ((s = stdError.readLine()) != null) {
-				System.out.println(s);
-				print += s + "\n<br/>";
-			}
-
-			// System.exit(0);
-		} catch (IOException e) {
-			LOG.warn("exception happened - here's what I know: ");
-			e.printStackTrace();
-//            System.exit(-1);
-		}
-
-		return print;
-
-	}
-
-	@GetMapping("exceldownload")
-	public void downloadFile(@RequestParam String file) throws IOException {
-
-		InputStream inputStream = new URL("http://app06.aspsols.com:8080/portal/www/" + file).openStream();
-		Files.copy(inputStream, Paths.get("/Users/erichaag/Downloads/" + file), StandardCopyOption.REPLACE_EXISTING);
-
-	}
-
-	@GetMapping("/user/list")
-	public void List() throws IOException {
-		srv.registrar("Esttt");
-	}
 
 	@PostMapping("/uploadfile") 
 	private String uploadFile(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
